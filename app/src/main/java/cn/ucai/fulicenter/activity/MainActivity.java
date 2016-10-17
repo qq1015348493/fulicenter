df8ff84b94119a8af391fd17d5337c61a05fd102
qq1015348493 <tx1015348493@163.com>
2016-10-17 09:22:28 +0800

    碎片切换

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package cn.ucai.fulicenter.activity;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -64,18 +66,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.new_good:
                 mutual((RadioButton)v);
+                New_good new_good = new New_good();
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.linearlayout,new_good);
+                transaction.commit();
                 break;
             case R.id.boutique:
                 mutual((RadioButton) v);
+                Boutique boutique = new Boutique();
+                FragmentManager manager2 = getSupportFragmentManager();
+                FragmentTransaction transaction2 = manager2.beginTransaction();
+                transaction2.replace(R.id.linearlayout,boutique);
+                transaction2.addToBackStack(null);
+                transaction2.commit();
                 break;
             case R.id.cart:
                 mutual((RadioButton) v);
+                Cart cart = new Cart();
+                FragmentManager manager3 = getSupportFragmentManager();
+                FragmentTransaction transaction3 = manager3.beginTransaction();
+                transaction3.replace(R.id.linearlayout,cart);
+                transaction3.addToBackStack(null);
+                transaction3.commit();
                 break;
             case R.id.category:
                 mutual((RadioButton) v);
+                Category category = new Category();
+                FragmentManager manager4 = getSupportFragmentManager();
+                FragmentTransaction transaction4 = manager4.beginTransaction();
+                transaction4.replace(R.id.linearlayout,category);
+                transaction4.addToBackStack(null);
+                transaction4.commit();
                 break;
             case R.id.personal:
                 mutual((RadioButton) v);
+                Personal personal = new Personal();
+                FragmentManager manager5 = getSupportFragmentManager();
+                FragmentTransaction transaction5 = manager5.beginTransaction();
+                transaction5.replace(R.id.linearlayout,personal);
+                transaction5.addToBackStack(null);
+                transaction5.commit();
                 break;
         }
     }
